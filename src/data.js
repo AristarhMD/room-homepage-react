a1e983d45b19cbc243f1a1e1506e78b42f2ae353
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-09-15 21:17:19 +0100

    Initial commit
    This is initial commit

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const data = [{
+  header: "Discover innovative ways to decorate",
+  text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+  imageSmall:"./images/smalls/mobile-image-hero-1.jpg",
+  imageDesktop:"./images/desktop/desktop-image-hero-1.jpg"
+  },
+  {
+  header:"We are available all across the glob",
+  text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+  imageSmall:"./images/smalls/mobile-image-hero-2.jpg",
+  imageDesktop:"./images/desktop/desktop-image-hero-2.jpg"
+  },
+  {
+  header:"Manufactured with the best materials",
+  text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+  imageSmall:"./images/smalls/mobile-image-hero-3.jpg",
+  imageDesktop:"./images/desktop/desktop-image-hero-3.jpg"
+  }]
