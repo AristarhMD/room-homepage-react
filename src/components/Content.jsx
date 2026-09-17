d129a21c6dd18829871ad581535c465dade60a3c
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-09-17 22:20:33 +0100

    Added buttons for carousel
    Was added buttons for carousel.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,8 +4,19 @@ import data from "../data.js"
 export default function Content(){
   return (
     <main>
-      <section className=" relative flex overflow-hidden">
-        {data.map((item, idx) => <Article key={idx} {...item}/>)}
+      <section className="relative">
+        <div className="flex overflow-hidden">
+          {data.map((item, idx) => <Article key={idx} {...item}/>)}
+        </div>
+        <div className="absolute flex right-0 xl:right-1/2 top-[360px] md:top-[400px] xl:top-[534px] -translate-y-full xl:translate-x-full">
+          <button className="flex justify-center items-center cursor-pointer size-14 md:size-20 bg-black hover:bg-grey-800">
+          <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd"/></svg>
+          </button>
+
+          <button className="flex justify-center items-center cursor-pointer size-14 md:size-20 bg-black hover:bg-grey-800">
+          <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd"/></svg>
+          </button>
+        </div>
       </section>
     </main>
   )
--- src/components/reusable/Article.jsx
+++ src/components/reusable/Article.jsx
@@ -2,18 +2,20 @@ import ShopBtn from "./ShopBtn.jsx";
 
 export default function Article({imageDesktop,imageSmall,alt,header,text}) {
   return (
-    <article className="shrink-0 w-full flex items-center flex-col xl:flex-row gap-16 xl:gap-0">
-      <div className="relative w-full h-auto xl:basis-full overflow-hidden">
-        <picture className="flex items-center justify-center">
+    <article className="shrink-0 w-full flex items-center flex-col xl:flex-row gap-16 xl:gap-0 ">
+      <div className="relative w-full h-90 md:h-100 xl:h-133.5 xl:basis-1/2 overflow-hidden">
+        <picture className="block w-full h-full">
           <source media="(min-width:768px )" srcSet={imageDesktop} />
-          <img src={imageSmall} alt={alt} className="w-full h-auto" />
+          <img src={imageSmall} alt={alt} className="w-full h-full object-cover" />
         </picture>
-
       </div>
-      <div className="px-8 md:px-16 xl:px-20 text-left xl:basis-200">
-        <p className="mb-4 preset-2 md:preset-1 text-black">{header}</p>
-        <p className="mb-8 preset-3-m text-grey-500">{text}</p>
-        <ShopBtn />
+
+      <div className="px-8 md:px-16 xl:px-25 text-left xl:basis-1/2 flex justify-center">
+        <div className="w-full xl:w-[78%]">
+          <p className="mb-4 preset-2 md:preset-1 text-black">{header}</p>
+          <p className="mb-8 preset-3-m text-grey-500">{text}</p>
+          <ShopBtn />
+        </div>
       </div>
     </article>
   );
